797a2dc7d0ce6c1590e5349dc14ed3bbc8ac4a37
Krasner <aakrasner@gmail.com>
2026-05-29 01:47:56 +0000

    update default tf version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export TF_VER="${TF_VER:-2.20}"
+export TF_VER="${TF_VER:-2.19}"
 export CUDA_VER="${CUDA_VER:-12.5.1}"
 export UBUNTU_VER="${UBUNTU_VER:-22.04}"
 export PYTHON_VER="${PYTHON_VER:-3.11}"
@@ -11,5 +11,4 @@ sudo docker build \
     --build-arg CUDA_VER=$CUDA_VER \
     --build-arg UBUNTU_VER=$UBUNTU_VER \
     --build-arg PYTHON_VER=$PYTHON_VER \
-    --progress=plain \
     -t "krasnera/tensorflow-${TF_VER}-cuda-${CUDA_VER}-python-${PYTHON_VER}-ubuntu-${UBUNTU_VER}-aarch64:latest" .
